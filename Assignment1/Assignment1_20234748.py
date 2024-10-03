5c905c5774986b800b10c06ac78c15f301f1000e
karu-rress <nsun5274@gmail.com>
2024-10-03 22:10:12 +0900

    working on delete...

--- Assignment1/Assignment1_20234748.py
+++ Assignment1/Assignment1_20234748.py
@@ -1,5 +1,6 @@
 """
 #
+THIS IS A HARD-LINK FILE!!!
 # Database Design
 #
 # Assignment 1: B-Tree
@@ -119,15 +120,11 @@ class BTree:
             # x: [ A B D D ] -> [ A B C D ]
             x.keys[i + 1] = k
 
-            # TODO: Disk write here (x)
-
         # Case 2: if the node x is an internal node
         else:
             while i >= 0 and k < x.keys[i]:
                 i -= 1
             i += 1
-
-            # TODO: Disk read here (x.children[i])
 
             if len(x.children[i].keys) == (2 * self.t) - 1:
                 self.split_child(x, i)
@@ -146,7 +143,6 @@ class BTree:
         """
         i: int = 0
 
-        # TODO: sequential search to binary search
         while i < len(x.keys) and key > x.keys[i][0]:
             i += 1
 
@@ -156,7 +152,6 @@ class BTree:
         if x.leaf:  # failure: the key is not found
             return None
 
-        # TODO: Disk read here (x.children[i])
         return self.search_key(x.children[i], key, n+1)
 
     # B-Tree-Delete
@@ -166,20 +161,29 @@ class BTree:
         # delete the key k from  the B-tree
         # return: None
         """
+        x, i = 0, 0
+        try:
+            # Search for the key
+            r = self.search_key(self.root, k)
+            if r is None:
+                return
+            x, i = r
 
-        # Search for the key
-        r = self.search_key(self.root, k)
-        if r is None:
-            return
-        x, i = r
+            # Case 1: The key is originally in a leaf node
+            if x.leaf:
+                self.delete_leaf_node(x, i)
 
-        # Case 1: The key is originally in a leaf node
-        if x.leaf:
-            self.delete_leaf_node(x, i)
+            # Case 2: The key is originally in an internal node
+            else:
+                self.delete_internal_node(x, i)
 
-        # Case 2: The key is originally in an internal node
-        else:
-            self.delete_internal_node(x, i)
+        except IndexError:
+            print(f"인덱스 오류! '{k}': x={x.keys}, i={i}")
+        except ValueError:
+            print(f"찾는 값이 없음! '{k}': x={x.keys}, i={i}")
+        except Exception as e:
+            print(f"Couldn't remove key '{k}': x={x.keys}, i={i}")
+            print(f"Error: {e}")
 
 
     def delete_leaf_node(self, x: Node, i: int) -> None:
@@ -188,17 +192,12 @@ class BTree:
         # Case 1: The key is originally in a leaf node
         """
 
-        assert i < len(x.keys), "The index is out of range."
-        assert len(x.keys) >= self.t - 1, "The number of keys is less than t-1."
-
         # Case 1-1: x.n >= t, just delete the key
-        if len(x.keys) >= self.t:
-            # x: [ A B C D E ] -> [ A B D E ]
-            x.keys.pop(i)
+        # x: [ A B C D E ] -> [ A B D E ]
+        x.keys.pop(i)
 
         # Case 1-2: x.n == t-1. We can't just delete the key from x!
-        else:
-            x.keys.pop(i)
+        if len(x.keys) == self.t - 1:
             self.borrow_merge(x)
 
     def delete_internal_node(self, x: Node, i: int) -> None:
@@ -208,35 +207,17 @@ class BTree:
         # Trick: Transform this case into Case 1
         """
 
-        # i = 0, k = 33
-        k = x.keys[i]
+        # 1. find the predecessor of the node
+        # x: [.30.33.], pred_node: [ 31 32 ], pred_idx: 1
+        pred_node, pred_idx = self.find_predecessor(x, i)
+        pred_key = pred_node.keys[pred_idx]
 
-        # Predecessor case
-        if len(x.children[i].keys) >= self.t:
-            # 1. find the predecessor of the node
-            # x: [.30.33.], pred_node: [ 31 32 ], pred_idx: 1
-            pred_node, pred_idx = self.find_predecessor(x, i)
-            pred_key = pred_node.keys[pred_idx]
+        # 2. replace the key with the predecessor
+        # x: [.30.32.], pred_key: 33
+        x.keys[i], pred_key = pred_key, x.keys[i]
 
-            # 2. replace the key with the predecessor
-            # x: [.30.32.], pred_key: 33
-            x.keys[i], pred_key = pred_key, x.keys[i]
-
-            # 3. delete the predecessor from the leaf
-            self.delete_leaf_node(pred_node, pred_idx)
-
-        elif len(x.children[i + 1].keys) >= self.t:
-            succ_node, succ_idx = self.find_successor(x, i)
-            succ_key = succ_node.keys[succ_idx]
-
-            x.keys[i], succ_key = succ_key, x.keys[i]
-            self.delete_leaf_node(succ_node, succ_idx)
-
-        # Merge case
-        # TODO: is this right?
-        else:
-            x.keys.pop(i)
-            self.borrow_merge(x)
+        # 3. delete the predecessor and check Case 1
+        self.delete_leaf_node(pred_node, pred_idx)
 
     @staticmethod
     def find_predecessor(x: Node, i: int) -> tuple[Node, int]:
@@ -250,17 +231,7 @@ class BTree:
         return node, len(node.keys) - 1
 
     @staticmethod
-    def find_successor(x: Node, i: int) -> tuple[Node, int]:
-        """
-        # Find the successor of the key at index i in node x
-        # Find the leftmost key in the right!
-        """
-        node = x.children[i + 1]
-        while not node.leaf:
-            node = node.children[0]         # go to leftmost
-        return node, 0
-
-    def merge_sibling(self, x: Node, i: int) -> None:
+    def merge_sibling(x: Node, i: int) -> None:
         """
         # Merge the i-th child of x with its (i+1)-th sibling
         # x: Parent node
@@ -271,7 +242,7 @@ class BTree:
         right = i if i > 0 else i + 1
 
         # Deleted key is in the i-th child of x
-        # y: [ 25 ], right_child: [ ∅ ]
+        # y: [ 25 ], z: [ ∅ ]
         y = x.children[left]
         z = x.children[right]
 
@@ -289,19 +260,6 @@ class BTree:
         # x: [.28 33.] -> [.33.]
         x.keys.pop(left)
         x.children.pop(right)
-
-        # check if the parent node.n >= t
-        # if not, borrow or merge with the sibling
-        # if the parent node is the root and has no child node,
-        # set the merged node as the new root
-        # and decrease the height of the B-tree
-        if len(x.keys) >= self.t:
-            return
-
-        if x == self.root and len(x.keys) == 0:
-            self.root = y
-            return
-        self.borrow_merge(x)
 
     def borrow_merge(self, x: Node) -> None:
         """
@@ -323,6 +281,18 @@ class BTree:
         else:
             self.merge_sibling(parent, parent_idx)
 
+            # After deleting the key, parent node's key count is reduced
+            # So, we need to check the parent node again
+            if len(parent.keys) < self.t - 1:
+                self.borrow_merge(parent)
+
+            # However, if the parent is the root and has no child node,
+            # (1) set the merged node as the new root
+            # (2) decrease the height of the B-tree
+            if parent == self.root and len(parent.keys) == 0:
+                self.root = parent.children[0]
+
+
     @staticmethod
     def borrow_sibling(x: Node, i: int, j: int) -> None:
         """
@@ -330,7 +300,6 @@ class BTree:
         # i: the index of the node x in the parent node
         # j: the index of the sibling node in the parent node
         """
-        # assume: i = 1, sibling_idx = 0
 
         # sibling: [ 25 28 ], parent: [.30｡33.]
         sibling = x.parent.children[j]
@@ -474,7 +443,7 @@ def main():
 
             # 1. Insertion
             if num == 1:
-                t = 3  # minimum degree
+                t = 2  # minimum degree   (TODO: need to 3)
                 B = BTree(t)  # make an empty b-tree with the minimum degree t
 
                 insert_file: pd.DataFrame = get_file()
@@ -492,6 +461,35 @@ def main():
             # 4. End program
             elif num == 4:
                 sys.exit(1)
+
+            ## FOR DEBUG
+            elif num == 5:
+                node1 = Node(True); node1.keys = [[5, 5]]
+                node2 = Node(True); node2.keys = [[15, 15]]
+                node3 = Node(False); node3.keys = [[10, 10]]
+                node3.children = [node1, node2]
+                node1.parent = node2.parent = node3
+
+                node4 = Node(True); node4.keys = [[25, 25], [28, 28]]
+                node5 = Node(True); node5.keys = [[31, 31], [32, 32]]
+                node6 = Node(True); node6.keys = [[35, 35]]
+                node7 = Node(False); node7.keys = [[30, 30], [33, 33]]
+                node7.children = [node4, node5, node6]
+                node4.parent = node5.parent = node6.parent = node7
+
+                node8 = Node(True); node8.keys = [[45, 45]]
+                node9 = Node(True); node9.keys = [[55, 55]]
+                node10 = Node(True); node10.keys = [[65, 65]]
+                node11 = Node(False); node11.keys = [[50, 50], [60, 60]]
+                node11.children = [node8, node9, node10]
+                node8.parent = node9.parent = node10.parent = node11
+
+                node12 = Node(False); node12.keys = [[20, 20], [40, 40]]
+                node12.children = [node3, node7, node11]
+                node3.parent = node7.parent = node11.parent = node12
+
+                B = BTree(2)
+                B.root = node12
 
             else:
                 print("Invalid input. Please enter 1, 2, 3, or 4.")
